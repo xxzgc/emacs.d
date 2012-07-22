27646102d13bd5dbfe8de587553235f5ad73b022
Taras <truestyler@gmail.com>
2012-07-23 01:20:08 +0300

    skip the minor mode in buffers where it is not applicable

--- init-yasnippet.el
+++ init-yasnippet.el
@@ -1,16 +1,26 @@
 ;; {{
 ;; yasnippet
-(add-to-list 'load-path
-              "~/.emacs.d/packages/yasnippet")
+(add-to-list 'load-path "~/.emacs.d/packages/yasnippet")
 (require 'yasnippet) ;; not yasnippet-bundle
+
 ;; (yas/initialize)
+
+;; (setq yas/snippet-dirs "~/.emacs.d/snippets") 
 (yas/load-directory "~/.emacs.d/snippets")
+
 (yas/global-mode 1)
 
 (require 'dropdown-list)
 (setq yas/prompt-functions '(yas/dropdown-prompt
                              yas/ido-prompt
                              yas/completing-prompt))
+
+;; Skip the minor mode in buffers where it is not applicable
+;; (no snippet tables)
+(set-default 'yas/dont-activate
+           #'(lambda ()
+               (and yas/snippet-dirs
+                    (null (yas/get-snippet-tables)))))
 
 ;; }}
 
