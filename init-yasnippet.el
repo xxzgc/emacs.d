a5cbd834cfe71444170010f9d014b4b348e0f784
Taras <truestyler@gmail.com>
2012-07-22 13:06:37 +0300

    upgrade `yasnippet`

--- init-yasnippet.el
+++ init-yasnippet.el
@@ -1,7 +1,7 @@
 ;; {{
 ;; yasnippet
 (add-to-list 'load-path
-              "~/.emacs.d/packages/yasnippet-0.6.1c")
+              "~/.emacs.d/packages/yasnippet")
 (require 'yasnippet) ;; not yasnippet-bundle
 (yas/initialize)
 (yas/load-directory "~/.emacs.d/snippets")
