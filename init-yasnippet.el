06c71b4088dc6f6dacadb37c13f8d18c4418b10d
Taras <truestyler@gmail.com>
2012-07-23 00:30:58 +0300

    yasnippet: add `yas/prompt-functions`

--- init-yasnippet.el
+++ init-yasnippet.el
@@ -3,8 +3,15 @@
 (add-to-list 'load-path
               "~/.emacs.d/packages/yasnippet")
 (require 'yasnippet) ;; not yasnippet-bundle
-(yas/initialize)
+;; (yas/initialize)
 (yas/load-directory "~/.emacs.d/snippets")
+(yas/global-mode 1)
+
+(require 'dropdown-list)
+(setq yas/prompt-functions '(yas/dropdown-prompt
+                             yas/ido-prompt
+                             yas/completing-prompt))
+
 ;; }}
 
 (provide 'init-yasnippet)
