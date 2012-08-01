6624d0aa5e544274ec87e1dd82cdbed5c7014766
Taras Yagniuk <mrtaryk@gmail.com>
2012-08-02 00:22:55 +0300

    init-clojure: paredit-mode

--- init-clojure.el
+++ init-clojure.el
@@ -1,5 +1,10 @@
 (add-to-list 'load-path "~/.emacs.d/packages/clojure-mode")
 
+(defun turn-on-paredit ()
+  (paredit-mode 1))
+
+(add-hook 'clojure-mode-hook 'turn-on-paredit)
+
 (require 'clojure-mode)
 
 (provide 'init-clojure)
