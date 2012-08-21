45b889c35fdaf9bc55c8086636e0f5119fc1f89c
Taras Yagniuk <mrtaryk@gmail.com>
2012-08-22 01:50:43 +0300

    .emacs: minor changes

--- init-common-lisp.el
+++ init-common-lisp.el
@@ -1,10 +1,8 @@
-(setq inferior-lisp-program "/usr/bin/sbcl") ; your Lisp system
 (add-to-list 'load-path "~/.emacs.d/packages/slime/") ; your SLIME directory
+(setq inferior-lisp-program "/usr/bin/sbcl") ; your Lisp system
 (require 'slime)
 (slime-setup)
-(load (expand-file-name "~/quicklisp/slime-helper.el"))
 
-;; Replace "sbcl" with the path to your implementation
-(setq inferior-lisp-program "sbcl")
+(load (expand-file-name "~/quicklisp/slime-helper.el"))
 
 (provide 'init-common-lisp)
