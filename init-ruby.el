8f10b04fd2314671adfd9adb0625c42f1e6afae2
Taras Yagniuk <mrtaryk@gmail.com>
2012-08-22 01:52:32 +0300

    .emacs: commented mode setting for ruby-files

--- init-ruby.el
+++ init-ruby.el
@@ -1,8 +1,8 @@
-;; {{
-;; ruby mode
-(set-mode-for-filename-patterns 
-  'ruby-mode
-  '("\\.rb$" "\\.rsel$" "\\.rhtml$" "\\.erb$" "\\.prawn$" "Rakefile$" "Gemfile$"))
-;; }}
+;; ;; {{
+;; ;; ruby mode
+;; (set-mode-for-filename-patterns 
+;;   'ruby-mode
+;;   '("\\.rb$" "\\.rsel$" "\\.rhtml$" "\\.erb$" "\\.prawn$" "Rakefile$" "Gemfile$"))
+;; ;; }}
 
 (provide 'init-ruby)
