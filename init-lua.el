4a255c0ec3bfd07c96733f9132d66777bf1b8164
Taras Yagniuk <mrtaryk@gmail.com>
2012-08-22 01:56:49 +0300

    .emacs: commented mode setting for lua-files

--- init-lua.el
+++ init-lua.el
@@ -1,7 +1,7 @@
 ;; {{
 ;; lua mode
 (setq load-path (cons "~/.emacs.d/packages/lua-mode" load-path))
-(setq auto-mode-alist (cons '("\\.lua$" . lua-mode) auto-mode-alist))
+;; (setq auto-mode-alist (cons '("\\.lua$" . lua-mode) auto-mode-alist))
 (autoload 'lua-mode "lua-mode" "Lua editing mode." t)
 ;; }}
 
