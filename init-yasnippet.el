42b7fe5da8c56e3e73b8a128a2f17bf598926e3d
Taras Iagniuk <mrtaryk@gmail.com>
2013-06-02 23:12:14 +0300

    Update `yasnippet` (rename `yas/` to `yas-`)

--- init-yasnippet.el
+++ init-yasnippet.el
@@ -3,27 +3,27 @@
 (add-to-list 'load-path "~/.emacs.d/packages/yasnippet")
 (require 'yasnippet) ;; not yasnippet-bundle
 
-(setq yas/snippet-dirs
+(setq yas-snippet-dirs
       '("~/.emacs.d/snippets"                     ;; personal snippets
         "~/.emacs.d/packages/yasnippet/snippets"  ;; the default collection
        ))
-;; (yas/initialize)
-;; (yas/load-directory "~/.emacs.d/snippets")
+;; (yas-initialize)
+;; (yas-load-directory "~/.emacs.d/snippets")
 
 (require 'dropdown-list)
-(setq yas/prompt-functions '(yas/x-prompt
-                             yas/dropdown-prompt
-                             yas/ido-prompt
-                             yas/completing-prompt))
+(setq yas-prompt-functions '(yas-x-prompt
+                             yas-dropdown-prompt
+                             yas-ido-prompt
+                             yas-completing-prompt))
 
 ;; Skip the minor mode in buffers where it is not applicable
 ;; (no snippet tables)
-(set-default 'yas/dont-activate
-           #'(lambda ()
-               (and yas/snippet-dirs
-                    (null (yas/get-snippet-tables)))))
+;;(set-default 'yas-dont-activate
+;;           #'(lambda ()
+;;               (and yas-snippet-dirs
+;;                    (null (yas-get-snippet-tables)))))
 
-(yas/global-mode 1)
+(yas-global-mode 1)
 
 (provide 'init-yasnippet)
 ;; }}
