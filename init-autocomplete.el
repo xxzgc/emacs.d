82db7f3093b60fafe29ee16bd1fa7358ec909809
Taras Iagniuk <mrtaryk@gmail.com>
2013-06-02 22:18:27 +0300

    Add `popup` package

--- init-autocomplete.el
+++ init-autocomplete.el
@@ -1,8 +1,16 @@
 ;; {{
 ;; auto completion mode
+(add-to-list 'load-path "~/.emacs.d/packages/popup/")
+(require 'popup)
+
+(add-to-list 'load-path "~/.emacs.d/packages/auto-complete/")
+
+(require 'auto-complete)
 (require 'auto-complete-config)
-(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict/")
 (ac-config-default)
+
+(auto-complete-mode t)
 ;; }}
 
 (provide 'init-autocomplete)
