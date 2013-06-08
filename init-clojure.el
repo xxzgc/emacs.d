eaad05ba3b71b17393faaf285b1faec4dfe9277c
Taras Iagniuk <mrtaryk@gmail.com>
2013-06-08 06:07:38 +0300

    Paredit mode for clojure is now defined in `init-paredit.el`

--- init-clojure.el
+++ init-clojure.el
@@ -1,10 +1,5 @@
 (add-to-list 'load-path "~/.emacs.d/packages/clojure-mode")
 
-(defun turn-on-paredit ()
-  (paredit-mode 1))
-
-(add-hook 'clojure-mode-hook 'turn-on-paredit)
-
 (require 'clojure-mode)
 
 (provide 'init-clojure)
