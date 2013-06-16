ea630f47bd96852716bc75a70793910786939554
Taras Iagniuk <mrtaryk@gmail.com>
2013-06-17 00:52:16 +0300

    Bind 'M-F3' to `highlight-symbol-query-replace`

--- init-highlight.el
+++ init-highlight.el
@@ -4,7 +4,6 @@
 (global-set-key [(control f3)] 'highlight-symbol-at-point)
 (global-set-key [f3] 'highlight-symbol-next)
 (global-set-key [(shift f3)] 'highlight-symbol-prev)
-(global-set-key [(meta f3)] 'highlight-symbol-prev)
-(global-set-key [(control meta f3)] 'highlight-symbol-query-replace)
+(global-set-key [(meta f3)] 'highlight-symbol-query-replace)
 
 (provide 'init-highlight)
