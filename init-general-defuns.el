c402391108c30cee0523794db2b83fda2e8a7383
Taras Iagniuk <mrtaryk@gmail.com>
2013-07-06 06:42:00 +0300

    Generate random data (string, hex, number)

--- init-general-defuns.el
+++ init-general-defuns.el
@@ -349,4 +349,35 @@ If point was already at that position, move point to beginning of line."
     (error (message "Invalid expression")
            (insert (current-kill 0)))))
 
+;; 
+;; Generate random data
+;; originally from http://ergoemacs.org/emacs/elisp_insert_random_number_string.html
+;; 
+
+(random t) ; seed it randomly
+
+(defun insert-random-number (max-number)
+  "Insert a random number between 0 to 999999."
+  (interactive "Max: ")
+  (insert (number-to-string (random max-number))))
+
+(defun insert-random-hex ()
+  "Insert a random 4-digit hexidecimal number."
+  (interactive)
+  (let (myCharset (possibleCharsCount 16))
+    (setq myCharset "1234567890abcdef" )
+    (dotimes (ii 4)
+      (insert (elt myCharset (random possibleCharsCount))) ) )
+  ;; (insert (format "%4x" (random 65535)) )
+  )
+
+(defun insert-random-string ()
+  "Insert a random alphanumerics string of length 5.
+The possible chars are 0 to 9, and a to z (lower case)."
+  (interactive)
+  (let (myCharset (possibleCharsCount 36))
+    (setq myCharset "1234567890abcdefghijklmnopqrstuvwxyz" )
+    (dotimes (ii 5)
+      (insert (elt myCharset (random possibleCharsCount))) ) ) )
+
 (provide 'init-general-defuns)
