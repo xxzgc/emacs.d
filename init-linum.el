68cbcf528c1953c20f72627dec051730dde0d69d
Taras Iagniuk <mrtaryk@gmail.com>
2013-07-12 17:47:23 +0300

    Customize linum format

--- init-linum.el
+++ init-linum.el
@@ -20,4 +20,22 @@
 ;; (setq linum-relative-current-symbol "->")
 ;; (require 'linum-relative)
 
+(eval-after-load 'linum
+  '(progn
+     (defface linum-leading-zero
+       `((t :inherit 'linum
+            :foreground ,(face-attribute 'linum :background nil t)))
+       "Face for displaying leading zeroes for line numbers in display margin."
+       :group 'linum)
+
+     (defun linum-format-func (line)
+       (let ((w (length
+                 (number-to-string (count-lines (point-min) (point-max))))))
+         (concat
+          (propertize (make-string (- w (length (number-to-string line))) ?0)
+                      'face 'linum-leading-zero)
+          (propertize (number-to-string line) 'face 'linum))))
+
+     (setq linum-format 'linum-format-func)))
+
 (provide 'init-linum)
