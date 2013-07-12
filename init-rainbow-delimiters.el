cbed73e28f7747b5dc1489a34ea2ddd6d4eb4a35
Taras Iagniuk <mrtaryk@gmail.com>
2013-07-12 11:47:58 +0300

    Load `rainbow-delimiters-mode` for `eval-expression-minibuffer-setup-hook`, `ielm-mode-hook`, and `lisp-interaction-mode-hook`

--- init-rainbow-delimiters.el
+++ init-rainbow-delimiters.el
@@ -3,7 +3,9 @@
 (add-to-list 'load-path "~/.emacs.d/packages/rainbow-delimiters")
 (require 'rainbow-delimiters)
 
-(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+(add-hook 'eval-expression-minibuffer-setup-hook 'rainbow-delimiters-mode)
+(add-hook 'ielm-mode-hook             'rainbow-delimiters-mode)
+(add-hook 'lisp-interaction-mode-hook 'rainbow-delimiters-mode)
 
 (provide 'init-rainbow-delimiters)
 ;;; init-rainbow-delimiters.el ends here
