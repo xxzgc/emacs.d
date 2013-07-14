2949d11551b8404ad14027498915afb81cf10120
Taras Iagniuk <mrtaryk@gmail.com>
2013-07-15 01:13:48 +0300

    Load the packages on demand if they are denoted as defer

--- init-package.el
+++ init-package.el
@@ -1,31 +1,32 @@
-(defun* use-custom-package (&key add-path package-name mode minor-modes defer line-numbers init-package)
+(defun* use-custom-package (&key add-path package-name mode minor-modes defer line-numbers init-package init)
   (if add-path
       (if (listp add-path)
           (dolist (path-item add-path)
             (add-to-list 'load-path path-item))
         (add-to-list 'load-path add-path)))
+  (when init
+    (eval init))
+  (when init-package
+    (let ((package-s (intern (concat "init-" (symbol-name package-name)))))
+      (if (and (not defer)
+               (not (featurep package-s)))
+          (require package-s))))
   (dolist (item mode)
     (mapcar
       (lambda (filename-pattern)
-        (setq 
-          auto-mode-alist 
-          (cons (cons filename-pattern (car item)) auto-mode-alist)))
+        `(add-to-list 'auto-mode-alist '(,filename-pattern . ,(car item))))
       (cdr item))
+    (when defer
+      (eval-after-load (symbol-name (car item))
+         `(let ((package-s (intern ,(concat "init-" (symbol-name package-name)))))
+            (unless (featurep package-s)
+              (require package-s)))))
     (if (listp minor-modes)
         (dolist (mmode minor-modes)
-          (lexical-let ((lmmode mmode))
-              (add-hook (intern (concat (symbol-name (car item)) "-hook"))
-                        (lambda ()
-                          (if (boundp lmmode)
-                              (funcall lmmode)
-                            (warn "%s isn't defined" lmmode))))))
+          `(add-hook (intern (concat (symbol-name (car item)) "-hook")) ,mmode))
       (warn "minor-modes should be list"))
     (when line-numbers
       (add-hook (intern (concat (symbol-name (car item)) "-hook"))
-                (lambda() (linum-mode)))))
-  (if init-package
-      (let ((package-s (intern (concat "init-" (symbol-name package-name)))))
-        (unless (featurep package-s)
-          (require package-s)))))
+                (lambda() (linum-mode))))))
 
 (provide 'init-package)
