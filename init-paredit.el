8feed779356bb4cea80554aef8559e20212d46e4
Taras Iagniuk <mrtaryk@gmail.com>
2013-07-15 01:19:30 +0300

    Autoload: paredit-mode

--- init-paredit.el
+++ init-paredit.el
@@ -3,10 +3,8 @@
 
 ;; (add-to-list 'load-path "~/.emacs.d/packages/paredit")
 
-;; (autoload 'enable-paredit-mode "paredit"
-;;   "Turn on pseudo-structural editing of Lisp code." t)
-
-(require 'paredit)
+(autoload 'paredit-mode "paredit"
+  "Turn on pseudo-structural editing of Lisp code." t)
 
 (add-hook 'eval-expression-minibuffer-setup-hook 'paredit-mode)
 (add-hook 'ielm-mode-hook             'paredit-mode)
