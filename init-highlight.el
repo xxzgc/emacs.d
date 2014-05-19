cf932f0f8afdf18e06a3c6f1e5f94cc1072fdf41
Taras Iagniuk <mrtaryk@gmail.com>
2014-05-19 17:28:29 +0300

    Change colour of a highlighted background

--- init-highlight.el
+++ init-highlight.el
@@ -5,6 +5,7 @@
 
 (setq hlt-auto-faces-flag 1)
 (setq highlight-symbol-idle-delay 1)
+(set-face-background 'highlight-symbol-face "gray36")
 
 (global-set-key [(control f3)] 'highlight-symbol-at-point)
 (global-set-key [f3] 'highlight-symbol-next)
