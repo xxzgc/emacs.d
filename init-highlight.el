5e51bc9f71452f3cb07b068b0d00cd931ee88e20
Taras Iagniuk <mrtaryk@gmail.com>
2014-05-19 16:49:46 +0300

    Set hlt-auto-faces-flag and highlight-symbol-idle-delay

--- init-highlight.el
+++ init-highlight.el
@@ -1,6 +1,11 @@
 ;; (add-to-list 'load-path "~/.emacs.d/packages/highlight-symbol/")
 
+(require 'highlight)
 (require 'highlight-symbol)
+
+(setq hlt-auto-faces-flag 1)
+(setq highlight-symbol-idle-delay 1)
+
 (global-set-key [(control f3)] 'highlight-symbol-at-point)
 (global-set-key [f3] 'highlight-symbol-next)
 (global-set-key [(shift f3)] 'highlight-symbol-prev)
