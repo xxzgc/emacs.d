35ccf4264e035f892a4bf242f17603baa48de513
Taras Iagniuk <mrtaryk@gmail.com>
2015-05-01 18:37:37 +0300

    Use persp-mode instead of perspective.el, as it is capable of saving/restoring perspectives

--- init/init-perspective.el
+++ init/init-perspective.el
@@ -1,75 +1,82 @@
-;; (add-to-list 'load-path "~/.emacs.d/packages/perspective")
+(add-to-list 'load-path "~/.emacs.d/packages/workgroups")
+(require 'workgroups)
+
+(add-to-list 'load-path "~/.emacs.d/packages/persp-mode")
+
+(setq persp-keymap-prefix (kbd "C-x x"))
+
 ;; Load Perspective
-(require 'perspective)
+(require 'persp-mode)
 ;; Toggle the perspective mode
-(persp-mode)
 
-(defmacro custom-persp (name &rest body)
-  `(let ((initialize (not (gethash ,name perspectives-hash)))
-         (current-perspective persp-curr))
-     (persp-switch ,name)
-     (when initialize ,@body)
-     (setq persp-last current-perspective)))
+(persp-mode t)
 
-(defun custom-persp-last ()
-  (interactive)
-  (persp-switch (persp-name persp-last)))
+;; (defmacro custom-persp (name &rest body)
+;;   `(let ((initialize (not (gethash ,name perspectives-hash)))
+;;          (current-perspective persp-curr))
+;;      (persp-switch ,name)
+;;      (when initialize ,@body)
+;;      (setq persp-last current-perspective)))
 
-(defun custom-persp/main ()
-  (interactive)
-  (custom-persp "@main"))
+;; (defun custom-persp-last ()
+;;   (interactive)
+;;   (persp-switch (persp-name persp-last)))
 
-(defun custom-persp/perl ()
-  (interactive)
-  (custom-persp "@perl"
-                (sepia-repl)))
+;; (defun custom-persp/main ()
+;;   (interactive)
+;;   (custom-persp "@main"))
 
-(defun custom-persp/term ()
-  (interactive)
-  (custom-persp "@term"
-                (multi-term)))
+;; (defun custom-persp/perl ()
+;;   (interactive)
+;;   (custom-persp "@perl"
+;;                 (sepia-repl)))
 
-(defun custom-persp/emacs ()
-  (interactive)
-  (custom-persp "@emacs"
-                (dolist (elfile '("~/.emacs.d/init.el"
-                                  "~/.emacs.d/custom.el"))
-                  (find-file elfile))))
+;; (defun custom-persp/term ()
+;;   (interactive)
+;;   (custom-persp "@term"
+;;                 (multi-term)))
 
-(defun custom-persp/org ()
-  (interactive)
-  (custom-persp "@org"
-                (dolist (orgfile org-agenda-files)
-                  (find-file orgfile))))
+;; (defun custom-persp/emacs ()
+;;   (interactive)
+;;   (custom-persp "@emacs"
+;;                 (dolist (elfile '("~/.emacs.d/init.el"
+;;                                   "~/.emacs.d/custom.el"))
+;;                   (find-file elfile))))
 
-(defun custom-persp/irc ()
-  (interactive)
-  (custom-persp "@irc"
-                (erc-perlorg)
-                (erc-freenode)))
+;; (defun custom-persp/org ()
+;;   (interactive)
+;;   (custom-persp "@org"
+;;                 (dolist (orgfile org-agenda-files)
+;;                   (find-file orgfile))))
+
+;; (defun custom-persp/irc ()
+;;   (interactive)
+;;   (custom-persp "@irc"
+;;                 (erc-perlorg)
+;;                 (erc-freenode)))
                 
-(global-set-key (kbd "M-p o") 'custom-persp/org)
-(global-set-key (kbd "M-p i") 'custom-persp/irc)
-(global-set-key (kbd "M-p t") 'custom-persp/term)
-(global-set-key (kbd "M-p p") 'custom-persp/perl)
-(global-set-key (kbd "M-p e") 'custom-persp/emacs)
-(global-set-key (kbd "M-p m") 'custom-persp/main)
+;; (global-set-key (kbd "M-p o") 'custom-persp/org)
+;; (global-set-key (kbd "M-p i") 'custom-persp/irc)
+;; (global-set-key (kbd "M-p t") 'custom-persp/term)
+;; (global-set-key (kbd "M-p p") 'custom-persp/perl)
+;; (global-set-key (kbd "M-p e") 'custom-persp/emacs)
+;; (global-set-key (kbd "M-p m") 'custom-persp/main)
 
-(defun open-persp (name buffer1 buffer2 &rest buffers)
-  (persp-switch name)
-  (delete-other-windows)
-  (split-window-right)
-  (let ((p (if (boundp 'prefix) prefix "")))
-    (let ((f1 (concat p buffer1))
-          (f2 (concat p buffer2)))
-      (when (file-exists-p f1)
-        (find-file f1))
-      (select-window (window-next-sibling))
-      (when (file-exists-p f2)
-        (find-file f2)))
-    (dolist (b buffers)
-      (let ((pb (concat p b)))
-        (when (file-exists-p pb)
-          (find-file pb))))))
+;; (defun open-persp (name buffer1 buffer2 &rest buffers)
+;;   (persp-switch name)
+;;   (delete-other-windows)
+;;   (split-window-right)
+;;   (let ((p (if (boundp 'prefix) prefix "")))
+;;     (let ((f1 (concat p buffer1))
+;;           (f2 (concat p buffer2)))
+;;       (when (file-exists-p f1)
+;;         (find-file f1))
+;;       (select-window (window-next-sibling))
+;;       (when (file-exists-p f2)
+;;         (find-file f2)))
+;;     (dolist (b buffers)
+;;       (let ((pb (concat p b)))
+;;         (when (file-exists-p pb)
+;;           (find-file pb))))))
 
 (provide 'init-perspective)
